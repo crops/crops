a7e6bcd502376b60d0af2ed32147ecb784e43c78
Todor Minchev <todor.minchev@linux.intel.com>
2016-02-13 07:59:17 -0800

    codi: remove container launcher from codi
    Signed-off-by: Todor Minchev <todor.minchev@linux.intel.com>

--- codi/codi.c
+++ codi/codi.c
@@ -26,14 +26,11 @@
 #include "codi_list.h"
 #include "codi_launcher.h"
 
-int codi_sock_fd;
 extern char *codi_ops[];
-static int cli_sock_fd;
-struct sockaddr cli_addr;
-socklen_t cli_len;
-static struct sockaddr_in *cli_ip;
-static char *cli_params[KEY_ARR_SZ], *ip;
+int codi_sock_fd;
+/*
 pthread_mutex_t lock;
+*/
 
 /*TODO - close sockets on CTRL+C. Get rid of this when running as a daemon */
 void close_sockets(int dummy) {
@@ -42,54 +39,20 @@ void close_sockets(int dummy) {
   exit(0);
 }
 
-static void *listener_thread(void *args)
-{
-  int i;
-  while (1) {
-    cli_sock_fd = accept(codi_sock_fd, &cli_addr, &cli_len);
-
-    if (cli_sock_fd < 0)
-      ERR("ERROR on accept");
-
-    pthread_mutex_lock(&lock);
-    free_params(cli_params);
-    receive_args(cli_sock_fd, cli_params);
-
-    /*turff registration*/
-    if (!strcmp(cli_params[KEY('z')], TURFF_NAME)) {
-      cli_ip = (struct sockaddr_in*) &cli_addr;
-      asprintf(&ip, "%s", inet_ntoa(cli_ip->sin_addr));
-      cli_params[KEY('c')] = ip;
-      db_insert_node(cli_params[KEY('n')], cli_params[KEY('c')],  cli_params[KEY('s')]);
-      INFO("\nToolchain registration received: %s : %s : %s \n", cli_params[KEY('n')],
-        cli_params[KEY('c')], cli_params[KEY('s')]) ;
-      close(cli_sock_fd);
-    } else if (!strcmp(cli_params[KEY('z')], CEED_NAME) && (cli_params[KEY('l')] != NULL )) {
-      /* ceed request for available toolchains */
-      return_turff_nodes(cli_sock_fd);
-      close(cli_sock_fd);
-    }
-
-    pthread_mutex_unlock(&lock);
-  }
-  return NULL;
-}
-
 int main(int argc, char *argv[]) {
   struct addrinfo *addr_p;
-  int i, cli_sock_fd_tmp, process_ok;
+  int i, cli_sock_fd;
   const char *codi_port = (const char*) CODI_PORT;
-  char *ceed_params_tmp[KEY_ARR_SZ];
-  char *docker_url;
-  pthread_t reg_toolchain_thread;
   turff_node *req_node = NULL;
+  char *cli_params[KEY_ARR_SZ], *ip, *docker_url, *tmp_str;
+  socklen_t cli_len;
+  struct sockaddr cli_addr;
+  struct sockaddr_in *cli_ip;
 
   parse_codi_params(argc, argv);
+
   for (i = 0; i <  KEY_ARR_SZ; i++)
     cli_params[i] = NULL;
-
-  if (pthread_mutex_init(&lock, NULL) != 0)
-    ERR("Mutex initialization failed\n");
 
   addr_p = bind_to_socket(NULL, codi_port, &codi_sock_fd);
 
@@ -105,65 +68,59 @@ int main(int argc, char *argv[]) {
 
   signal(SIGINT, close_sockets);
 
-  /*start toolchain registration thread*/
-  if (pthread_create(&reg_toolchain_thread, NULL, listener_thread, NULL)) {
-    ERR("Unable to start socket listener thread\n");
-  }
-
-  process_ok = 0;
-  for (i = 0; i <  KEY_ARR_SZ; i++)
-    ceed_params_tmp[i] = NULL;
-
   while(1) {
+    cli_sock_fd = accept(codi_sock_fd, &cli_addr, &cli_len);
 
-    pthread_mutex_lock(&lock);
+    if (cli_sock_fd < 0)
+      ERR("ERROR on accept");
 
-    if (cli_params[KEY('z')] != NULL && !process_ok && !strcmp(cli_params[KEY('z')], CEED_NAME)
-      && cli_params[KEY('d')] != NULL) {
+    free_params(cli_params);
+    receive_args(cli_sock_fd, cli_params);
 
-      if (ceed_params_tmp[KEY('z')] == NULL) {
-        copy_params(cli_params, ceed_params_tmp);
-        cli_sock_fd_tmp = dup(cli_sock_fd);
-      }
-
+    /*turff registration*/
+    if (!strcmp(cli_params[KEY('z')], TURFF_NAME)) {
+      cli_ip = (struct sockaddr_in*) &cli_addr;
+      asprintf(&ip, "%s", inet_ntoa(cli_ip->sin_addr));
+      cli_params[KEY('c')] = ip;
+      db_insert_node(cli_params[KEY('n')], cli_params[KEY('c')],  cli_params[KEY('s')]);
+      INFO("\nToolchain registration received: %s : %s : %s \n", cli_params[KEY('n')],
+      cli_params[KEY('c')], cli_params[KEY('s')]) ;
+    } else if (!strcmp(cli_params[KEY('z')], CEED_NAME) && (cli_params[KEY('l')] != NULL )) {
+      /* ceed request for available toolchains */
+      return_turff_nodes(cli_sock_fd);
+    } else if (!strcmp(cli_params[KEY('z')], CEED_NAME) && (cli_params[KEY('d')] != NULL )) {
       /* must be a command from ceed*/
       req_node = find_turff_node(cli_params[KEY('d')]);
-
-      /* check if docker engine is listening on a unix socket or tcp*/
-      if (codi_ops[KEY('i')] == NULL || codi_ops[KEY('p')] == NULL) {
-        asprintf(&docker_url, "%s", codi_ops[KEY('u')]);
-      } else {
-        asprintf(&docker_url, "%s:%s", codi_ops[KEY('i')], codi_ops[KEY('p')]);
-      }
-
       if (req_node != NULL) {
-        if (is_container_running(docker_url, cli_params[KEY('d')])) {
-          process_ok = 1;
-        } else if (start_container(docker_url, cli_params[KEY('d')])) {
-          process_ok = 1;
+
+        /* check if docker engine is listening on a unix socket or tcp*/
+        if (codi_ops[KEY('i')] == NULL || codi_ops[KEY('p')] == NULL) {
+          asprintf(&docker_url, "%s", codi_ops[KEY('u')]);
         } else {
-          INFO("Container %s does not exists\n", cli_params[KEY('d')]);
+          asprintf(&docker_url, "%s:%s", codi_ops[KEY('i')], codi_ops[KEY('p')]);
         }
-      } else if (start_container(docker_url, cli_params[KEY('d')])) {
-        process_ok = 1;
+
+        if (is_container_running(docker_url, cli_params[KEY('d')])) {
+          process_ceed_cmd(req_node, cli_sock_fd, cli_params);
+        } else {
+          asprintf(&tmp_str, "Container %s is not running\n%s",
+            cli_params[KEY('d')], TURFF_EOM);
+          send_data(cli_sock_fd, tmp_str, strlen(tmp_str));
+          free(tmp_str);
+        }
+        free(docker_url);
       } else {
-        INFO("Container %s not running\n", cli_params[KEY('d')]);
+        asprintf(&tmp_str, "Container %s not found in database\n%s",
+          cli_params[KEY('d')], TURFF_EOM);
+        send_data(cli_sock_fd, tmp_str, strlen(tmp_str));
+        free(tmp_str);
       }
-      free(docker_url);
+    } else {
+      asprintf(&tmp_str, "Invalid command sent to CODI\n%s", TURFF_EOM);
+      send_data(cli_sock_fd, tmp_str, strlen(tmp_str));
+      free(tmp_str);
     }
 
-    pthread_mutex_unlock(&lock);
-
-    if (cli_params[KEY('z')] != NULL && process_ok) {
-      req_node = find_turff_node(ceed_params_tmp[KEY('d')]);
-      if (req_node != NULL) {
-        process_ceed_cmd(req_node, cli_sock_fd_tmp, ceed_params_tmp);
-        process_ok = 0;
-        free_params(ceed_params_tmp);
-        free_params(cli_params);
-        close(cli_sock_fd_tmp);
-        close(cli_sock_fd);
-      }
-    }
+    free_params(cli_params);
   }
 }
--- codi/codi_launcher.c
+++ codi/codi_launcher.c
@@ -60,11 +60,7 @@ char *curl_request(char *http_method, char *url, char *api,  char *data)
     if (url[0] == '/') {
       /* map the unix socket as a connection endpoint */
       curl_easy_setopt(curl, CURLOPT_UNIX_SOCKET_PATH, url);
-
-      if (!strcmp(http_method, POST_REQUEST))
-        asprintf(&request_str, "%s%s", "http:", api);
-      else
-        asprintf(&request_str, "'%s%s'", "http:", api);
+      asprintf(&request_str, "%s%s", "http:", api);
     } else {
         asprintf(&request_str, "%s%s%s",  "http://", url, api);
     }
@@ -82,6 +78,7 @@ char *curl_request(char *http_method, char *url, char *api,  char *data)
       else
         curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
     }
+
     curl_easy_setopt(curl, CURLOPT_HEADER, 1);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *) &srv_reply);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, process_srv_reply);
@@ -125,30 +122,6 @@ size_t process_srv_reply(void *ptr, size_t size, size_t nmemb, void *reply_p)
   return chunk_size;
 }
 
-
-bool start_existing_container(char *url, char *cont_name)
-{
-  char *api_str = NULL, *result;
-
-  asprintf(&api_str, START_CONTAINER, cont_name);
-  result = curl_request(POST_REQUEST, url, api_str, NULL);
-  free(api_str);
-
-  if ((strstr(result, NO_ERROR_204) != NULL) ||
-    (strstr(result, NOT_MODIFIED_304) != NULL)) {
-    free(result);
-    return true;
-  } else {
-
-#ifdef DBG
-  INFO("Unable to start container : %s. Status: %s\n", cont_name, result);
-#endif
-
-    free(result);
-    return false;
-  }
-}
-
 bool is_container_running(char *url, char *cont_name)
 {
   char *api_str = NULL;
@@ -170,207 +143,4 @@ bool is_container_running(char *url, char *cont_name)
     json_decref(result);
     return false;
   }
-}
-
-bool container_exists(char *url, char *cont_name)
-{
-  char *api_str = NULL;
-  json_t *result;
-
-  asprintf(&api_str, NAMED_CONTAINER, cont_name);
-  result = request_json(GET_REQUEST, url, api_str, NULL);
-  free(api_str);
-
-  if (json_array_size(result)) {
-    json_decref(result);
-    return true;
-  } else {
-
-#ifdef DBG
-  INFO("Container : %s does not exist\n", cont_name);
-#endif
-
-    json_decref(result);
-    return false;
-  }
-}
-
-bool download_img(char *url, char *repo_tag)
-{
-  char *api_str = NULL, *repo, *tag, *tmp, *result;
-  asprintf(&repo, "%s", repo_tag);
-  tmp = strchr(repo, ':');
-
-  if (!tmp)
-    return false;
-
-  tag = tmp + 1 ;
-  *tmp = '\0';
-  asprintf(&api_str, GET_IMAGE, repo, tag);
-  result = curl_request(POST_REQUEST, url, api_str, NULL);
-  free(api_str);
-  free(repo);
-
-  if (strstr(result, PULL_COMPLETE) != NULL ||
-    strstr(result, IMAGE_UP_TO_DATE) != NULL ||
-    strstr(result, OK_200) != NULL) {
-    free(result);
-    return true;
-  } else {
-
-#ifdef DBG
-  INFO("Unable to download image : %s:%s. Status: %s\n", repo, tag, result);
-#endif
-
-    free(result);
-    return false;
-  }
-}
-
-bool create_new_container(char *url, char *img, char *host_dir)
-{
-  int i;
-  char *cont_params = NULL, *tmp_img, *qry_str, *result;
-
-char *cont_definition = CONTAINER_PARAMS(
-  {
-    "Hostname": "",
-    "Domainname": "",
-    "User": "",
-    "AttachStdin": false,
-    "AttachStdout": false,
-    "AttachStderr": false,
-    "Tty": false,
-    "OpenStdin": false,
-    "StdinOnce": false,
-    "Env": [
-      "TURFFID=%s"
-    ],
-    "Cmd": [
-      "/bin/crops"
-    ],
-    "Entrypoint": [
-      "/bin/turff_launcher",
-      "-f",
-      "/opt/poky/environment-setup*"
-    ],
-    "Image": "%s",
-    "Labels": null,
-    "Mounts": [
-      {
-        "Source": "%s",
-        "Destination": "/crops",
-        "Mode": "",
-        "RW": true
-      }
-    ],
-    "WorkingDir": "",
-    "NetworkDisabled": false,
-    "ExposedPorts": null,
-    "StopSignal": "SIGTERM",
-    "HostConfig": {
-      "Binds": ["%s:/crops"],
-      "Links": null,
-      "LxcConf": null,
-      "Memory": 0,
-      "MemorySwap": 0,
-      "MemoryReservation": 0,
-      "KernelMemory": 0,
-      "CpuShares": 0,
-      "CpuPeriod": 0,
-      "CpuQuota": 0,
-      "CpusetCpus": "",
-      "CpusetMems": "",
-      "BlkioWeight": 0,
-      "MemorySwappiness": null,
-      "OomKillDisable": false,
-      "PortBindings": null,
-      "PublishAllPorts": true,
-      "Privileged": false,
-      "ReadonlyRootfs": false,
-      "Dns": null,
-      "DnsOptions": null,
-      "DnsSearch": null,
-      "ExtraHosts": null,
-      "VolumesFrom": null,
-      "CapAdd": null,
-      "CapDrop": null,
-      "RestartPolicy": { "Name": "", "MaximumRetryCount": 0 },
-      "NetworkMode": "host",
-      "Devices": null,
-      "Ulimits": null,
-      "LogConfig": { "Type": "json-file", "Config": {} },
-      "SecurityOpt": null,
-      "CgroupParent": "",
-      "VolumeDriver": ""
-    }
-  }
-);
-
-  asprintf(&tmp_img, "%s", img);
-
-  /* container names cannot have special characters */
-  /* replace non alpha numberical characters with dashes */
-  for(i = 0; i < strlen(tmp_img); i++) {
-    if (!isalnum(tmp_img[i]))
-      tmp_img[i] = '-';
-  }
-
-
-  asprintf(&cont_params, cont_definition, img, img, host_dir, host_dir);
-  asprintf(&qry_str, CREATE_CONTAINER, tmp_img);
-  free(tmp_img);
-  result = curl_request(POST_REQUEST, url, qry_str, cont_params);
-  free(cont_params);
-  free(qry_str);
-
-
-
-  if (strstr(result, CREATED_201) != NULL) {
-    free(result);
-    return true;
-  } else {
-
-#ifdef DBG
-  INFO("Unable to create container : %s. Status: %s\n", img, result);
-#endif
-
-    free(result);
-    return false;
-  }
-}
-
-bool start_container(char *url, char *cont_name)
-{
-  int i;
-  char *tmp_name;
-
-  asprintf(&tmp_name, "%s", cont_name);
-
-  /* container names cannot have special characters */
-  /* replace non alpha numberical characters with dashes */
-  for (i = 0; i < strlen(tmp_name); i++) {
-    if (!isalnum(tmp_name[i]))
-      tmp_name[i] = '-';
-  }
-
-  if (container_exists(url, tmp_name)) {
-    if (start_existing_container(url, tmp_name)) {
-      free(tmp_name);
-      return true;
-    }
-  } else {
-    if (download_img(url, cont_name)) {
-      /* TODO host_dir will be different on Linux/Mac/Windows!!! */
-      if (create_new_container(url, cont_name, "/crops")) {
-        if (start_existing_container(url, tmp_name)) {
-          free(tmp_name);
-          return true;
-        }
-      }
-    }
-  }
-
-  free(tmp_name);
-  return false;
 }
