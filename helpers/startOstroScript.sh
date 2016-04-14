71b934daaa1075fbbf65b7a49d6ebf917606be3d
brian avery <brian.avery@intel.com>
2016-04-13 20:54:03 -0700

    helpers: add startOstroScript.sh
      1) sets up a yoctobuilder dynamic user that matches the uid
           of the shared dir
      2) puts an ostro.conf into the shared folder if one isn't there
      3) runs the runbitbake.py script using sudo as the yoctobuilder
           dynamic user
    
    Signed-off-by: brian avery <brian.avery@intel.com>

---
+++ helpers/startOstroScript.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+OSTRO_CONF="ostro.conf"
+POKY_DIR=/ostro/ostro-os/
+EXTRA_CONF=/ostro/ostro-shared/conf/ostro.conf
+BUILD_DIR=/ostro/build/
+
+H_UID=$(ls -al /ostro/ostro-shared/ | egrep "\."|egrep -v "\.\." | awk '{print $3}')
+H_USER=$(getent passwd "$H_UID" | cut -d: -f1)
+if [ "$H_USER" == "" ]; then
+    H_USER="yoctobuilder"
+    /usr/sbin/useradd -m -u ${H_UID} ${H_USER}
+fi
+
+# provide the ostro.conf if there isn't one yet
+if [ ! -f /ostro/ostro-shared/conf/${OSTRO_CONF} ]; then
+    mkdir -p /ostro/ostro-shared/conf
+    cp /ostro/sample-conf/ostro.conf /ostro/ostro-shared/conf/${OSTRO_CONF}
+fi
+chmod a+rwx /ostro/ostro-shared/conf
+chmod a+rw  /ostro/ostro-shared/conf/${OSTRO_CONF}
+
+
+# switch into user to build 
+sudo  --user ${H_USER} /ostro/bin/runbitbake.py --pokydir $POKY_DIR --extraconf $EXTRA_CONF -b $BUILD_DIR -t $*
+
+
