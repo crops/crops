2928cafff11b82cc04f062e7ede1525b30915f2a
brian avery <brian.avery@intel.com>
2016-04-25 14:05:42 -0700

    helpers: fix startup bugs in image copying code and layer addition code
      runbitbake.py: fix the way the extra layers and configurations are appended
      startOstroScript.sh: fix image copying code,add extralayers argument
    
    Signed-off-by: brian avery <brian.avery@intel.com>

--- helpers/startOstroScript.sh
+++ helpers/startOstroScript.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-OSTRO_CONF="ostro.conf"
 POKY_DIR=/ostro/ostro-os/
 EXTRA_CONF=/ostro/ostro-shared/conf/ostro.conf
 BUILD_DIR=/ostro/build/
+EXTRA_LAYERS=/ostro/ostro-shared/conf/ostro.layers
 
 H_UID=$(ls -al /ostro/ostro-shared/ | egrep "\."|egrep -v "\.\." | awk '{print $3}')
 H_USER=$(getent passwd "$H_UID" | cut -d: -f1)
@@ -12,17 +12,31 @@ if [ "$H_USER" == "" ]; then
 fi
 
 # provide the ostro.conf if there isn't one yet
-if [ ! -f /ostro/ostro-shared/conf/${OSTRO_CONF} ]; then
+if [ ! -f ${EXTRA_CONF} ]; then
     mkdir -p /ostro/ostro-shared/conf
-    cp /ostro/sample-conf/ostro.conf /ostro/ostro-shared/conf/${OSTRO_CONF}
+    cp /ostro/sample-conf/ostro.conf ${EXTRA_CONF}
 fi
+
+# provide ostro.layers if there isn't one yet
+if [ ! -f ${EXTRA_LAYERS} ]; then
+    mkdir -p /ostro/ostro-shared/conf
+    cp /ostro/sample-conf/ostro.layers ${EXTRA_LAYERS}
+fi
+
+mkdir -p /ostro/ostro-shared/images/
+chmod a+rwx /ostro/ostro-shared/images/
 chmod a+rwx /ostro/ostro-shared/conf
-chmod a+rw  /ostro/ostro-shared/conf/${OSTRO_CONF}
+chmod a+rw  ${EXTRA_CONF}
 
 
 # switch into user to build
-sudo  --user ${H_USER} /ostro/bin/runbitbake.py --pokydir $POKY_DIR --extraconf $EXTRA_CONF -b $BUILD_DIR -t $*
+sudo  --user ${H_USER} /ostro/bin/runbitbake.py --pokydir $POKY_DIR --extraconf $EXTRA_CONF --extralayers $EXTRA_LAYERS -b $BUILD_DIR -t $*
 echo "copying images to shared folder"
+
+# we need to source the bitbake env in order to use the bitbake script to find the images
+cd ${BUILD_DIR}
+source ${POKY_DIR}/oe-init-build-env >> /dev/null
 CON_DIR=`bitbake -e | egrep "DEPLOY_DIR_IMAGE\="|tr "\=" " " | tr -d "\""| awk '{print $2}'`
-rsync -a ${CON_DIR} /ostro/ostro-shared/images/
+rsync -a ${CON_DIR} /ostro/ostro-shared/images/ > /dev/null 2>&1
+
 
